b264e2bea536afdaad110de820a22ad0bb0080b2
Matthew Emes <memes@matthewemes.com>
2021-11-05 14:42:41 -0700

    Consolidate Cache implementations into cache.go/cache_test.go

--- v2/cache.go
+++ v2/cache.go
@@ -2,6 +2,8 @@ package pi
 
 import (
 	"context"
+
+	"github.com/gomodule/redigo/redis"
 )
 
 // Cache defines an interface for a cache implementation that can be used to
@@ -34,4 +36,53 @@ func (n *noopCache) SetValue(ctx context.Context, key string, value string) erro
 // dropped by SetValue and calls to GetValue always return an empty string.
 func NewNoopCache() *noopCache {
 	return &noopCache{}
+}
+
+// redisCache implements Cache interface with a Redis store.
+type redisCache struct {
+	*redis.Pool
+}
+
+// Return a new Cache implementation using Redis
+func NewRedisCache(ctx context.Context, endpoint string) *redisCache {
+	return &redisCache{
+		&redis.Pool{
+			DialContext: func(ctx context.Context) (redis.Conn, error) {
+				return redis.DialContext(ctx, "tcp", endpoint)
+			},
+		},
+	}
+}
+
+// Returns the string value stored in Redis under key, if present, or an empty string.
+func (r *redisCache) GetValue(ctx context.Context, key string) (string, error) {
+	l := logger.V(0).WithValues("key", key)
+	l.Info("GetValue: enter")
+	conn := r.Get()
+	defer conn.Close()
+
+	value, err := redis.String(conn.Do("GET", key))
+	if err == redis.ErrNil {
+		l.Info("Value is not cached")
+		return "", nil
+	}
+	if err != nil {
+		return "", err
+	}
+	l.Info("GetValue: exit", "value", value)
+	return value, nil
+}
+
+// Store the string key:value pair in Redis.
+func (r *redisCache) SetValue(ctx context.Context, key string, value string) error {
+	l := logger.V(0).WithValues("key", key, "value", value)
+	l.Info("SetValue: enter")
+	conn := r.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("SET", key, value)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- v2/cache_test.go
+++ v2/cache_test.go
@@ -3,7 +3,10 @@ package pi
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"testing"
+
+	"github.com/alicebob/miniredis"
 )
 
 const (
@@ -19,9 +22,9 @@ func TestNoopCache(t *testing.T) {
 	if cache == nil {
 		t.Error("Noop cache is nil")
 	}
-	for i := 0; i < TEST_CACHE_LOOP_LIMIT; i++ {
+	for i := uint64(0); i < TEST_CACHE_LOOP_LIMIT; i++ {
 		expected := ""
-		key := fmt.Sprintf("%d", i)
+		key := strconv.FormatUint(i, 16)
 		actual, err := cache.GetValue(ctx, key)
 		if err != nil {
 			t.Errorf("GetValue returned an error: %v", err)
@@ -30,6 +33,45 @@ func TestNoopCache(t *testing.T) {
 			t.Errorf("Index %d: Expected %s received %s", i, expected, actual)
 		}
 		if err = cache.SetValue(ctx, key, "1234"); err != nil {
+			t.Errorf("Index: %d: SetValue returned an error: %v", i, err)
+
+		}
+		actual, err = cache.GetValue(ctx, key)
+		if err != nil {
+			t.Errorf("GetDigits returned an error: %v", err)
+		}
+		if actual != expected {
+			t.Errorf("Index %d: Expected %s received %s", i, expected, actual)
+		}
+
+	}
+}
+
+// The RedisCache will use a Redis-like in-memory instance to cache values. The
+// test should confirm that a value can be added to the cache and recalled
+// successfully.
+func TestRedisCache(t *testing.T) {
+	ctx := context.Background()
+	mock, err := miniredis.Run()
+	if err != nil {
+		t.Errorf("Error running miniredis: %v", err)
+	}
+	cache := NewRedisCache(ctx, mock.Addr())
+	if cache == nil {
+		t.Error("Redis cache is nil")
+	}
+	for i := uint64(0); i < TEST_CACHE_LOOP_LIMIT; i++ {
+		expected := ""
+		key := strconv.FormatUint(i, 16)
+		actual, err := cache.GetValue(ctx, key)
+		if err != nil {
+			t.Errorf("GetValue returned an error: %v", err)
+		}
+		if actual != expected {
+			t.Errorf("Index %d: Expected %s received %s", i, expected, actual)
+		}
+		expected = fmt.Sprintf("%09d", i)
+		if err = cache.SetValue(ctx, key, expected); err != nil {
 			t.Errorf("Index: %d: SetValue returned an error: %v", i, err)
 
 		}
