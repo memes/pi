ca12eecb5e43649d6741fbe5e32e34d787206a68
Matthew Emes <memes@matthewemes.com>
2022-04-07 18:48:22 -0700

    OpenTelemetry utility support: optional TLS
     - default otel-all-in-one.yaml will launch OpenTelemetry collector with
       a plaintext gRPC listener, and embedded Jaeger and Prometheus.
     - default Makefile target will generate OTEL collector TLS certificates
       and create a kustomized kube yaml with the TLS certificates embedded.
     - all configuration is via ConfigMaps now that Podman supports it.

--- v2/supporting/otel-all-in-one.sh
+++ v2/supporting/otel-all-in-one.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 #
 # Quick script to run OpenTelemetry collector with Jaeger and Prometheus, in
-# podman with optional TLS support.
+# podman, with optional TLS support.
 
-# Shutdown existing deployment, if running
-test -z "$(podman pod ps --filter name=otel-all-in-one --format '{{ .Id }}')" || \
-    podman play kube --down otel-all-in-one.yaml
+LIBDIR="$(readlink -f `dirname $0`)"
+if [ -e "${LIBDIR}/otel-all-in-one-tls.yaml" ]; then
+    CONFIG="${LIBDIR}/otel-all-in-one-tls.yaml"
+else
+    CONFIG="${LIBDIR}/otel-all-in-one.yaml"
+fi
 
-# Create a volume for Prometheus and OpenTelemetry config files; podman play kube
-# doesn't support ConfigMap volume mounts, but does support using PVCs with volumes
-podman volume exists otel-all-in-one-prom-config || \
-    podman volume create otel-all-in-one-prom-config
-podman volume exists otel-all-in-one-otel-config || \
-    podman volume create otel-all-in-one-otel-config
-_container=$(podman create \
-    --mount type=volume,src=otel-all-in-one-prom-config,target=/mnt/prom-config \
-    --mount type=volume,src=otel-all-in-one-otel-config,target=/mnt/otel-config \
-    otel/opentelemetry-collector:0.46.0)
-tar cf - prometheus.yml | podman cp - "${_container}:/mnt/prom-config/"
-tar cf - otel-collector-config.yaml otel.pi.example.com.pem otel.pi.example.com-key.pem | \
-    podman cp - "${_container}:/mnt/otel-config"
-podman rm "${_container}"
+# Shutdown existing deployment, if running, and any existing ConfigMap volumes
+test -z "$(podman pod ps --filter name=$(basename "${CONFIG}" .yaml) --format '{{ .Id }}')" || \
+    podman play kube --down ${CONFIG}
+podman volume ls --format '{{ .Name }}' | \
+    grep -E '^otel-(?:[^-]+-config|collector-tls-[0-9a-z]+)$' | \
+    xargs -J % podman volume rm % --force
 
 # Launch OpenTelemetry, Prometheus and Jaeger
-podman play kube otel-all-in-one.yaml
+podman play kube ${CONFIG}
