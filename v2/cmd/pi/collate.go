f8b288831ef348ad9e5f0440dabcd5abd591b0b8
Matthew Emes <memes@matthewemes.com>
2023-01-22 21:10:57 -0800

    feat(collate): Add a client command to print pi
    For most use cases, the client is used to generate requests and the
    results are largely irrelevant. This feature adds a client subcommand
    `collate` that creates a target buffer and adds resulting digits to it
    as results are returned. This allows using the base client when collating
    the results are unneeded.

---
+++ v2/cmd/pi/collate.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// Implements the collate sub-command which collects the resulting values into
+// an output string.
+func NewCollateCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "collate target",
+		Short: "Run a gRPC Pi Service client to request and collate fractional digits of pi",
+		Long: `Launches a gRPC client that will connect to Pi Service target and request the fractional digits of pi.
+
+Metrics and traces will be sent to an OpenTelemetry collection endpoint, if specified.`,
+		Args: cobra.ExactArgs(1),
+		RunE: collateMain,
+	}
+}
+
+// Collate sub-command entrypoint. This function will create a byte array to
+// hold the results and a CollatorFunction to populate the byte array before
+// calling the main client loop.
+func collateMain(cmd *cobra.Command, endpoints []string) error {
+	count := viper.GetInt(CountFlagName)
+	logger := logger.V(1).WithValues(CountFlagName, count, "endpoints", endpoints)
+	logger.V(0).Info("Preparing target buffer")
+	digits := make([]byte, count)
+	for i := range digits {
+		digits[i] = '-'
+	}
+	// Set the global collator function to add digits to the array
+	collator = func(index uint64, value uint32) error {
+		digits[index] = '0' + byte(value)
+		return nil
+	}
+	if err := clientMain(cmd, endpoints); err != nil {
+		return err
+	}
+	fmt.Print("Result is: 3.")
+	if _, err := os.Stdout.Write(digits); err != nil {
+		return fmt.Errorf("failure writing result: %w", err)
+	}
+	fmt.Println()
+	return nil
+}
